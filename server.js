6450e93dc9945c050ae8c910524846d0e93ecf3b
srajesh636 <srajesh636@gmail.com>
2018-09-22 20:12:29 +0530

    simple server

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const chalk = require('chalk')
 const http = require('http')
 const port = 8080
 const server = http.createServer((req, res) => {
-  res.write('Hello World')
+  res.write('A Simple Server created using Node !')
   res.end()
 })
 
 server.listen(port, (error) => {
   error ? console.log(chalk.red('Compilation failed !'))
-    : 
-  console.log('\nCompiled successfully !')
+    : console.log('\nCompiled successfully !')
   console.log(chalk.yellow('\nYour Server is :'))
   console.log(chalk.cyan(`Running on port ${port} `))
 })
